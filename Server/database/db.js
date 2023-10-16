5b5af6b55d16a798feff5235a4722a6245665fb5
Fer-gi <gise.fernandez87@gmail.com>
2023-10-16 11:11:00 +0200

    feat (.env) create .env

--- Server/database/db.js
+++ Server/database/db.js
@@ -1,10 +1,18 @@
 import {Sequelize} from "sequelize";
+import dotenv from "dotenv";
 
+dotenv.config()
 
-
-const db = new Sequelize('db_montessori', 'root','Qwerty1987', {
-    host:'localhost',
-    dialect: 'mysql'
+const {
+    DB_NAME ,
+    DB_USER,
+    DB_PASSWORD,
+    DB_HOST,
+    DB_DIALECT
+}= process.env
+const db = new Sequelize(DB_NAME, DB_USER,DB_PASSWORD, {
+    host:DB_HOST,
+    dialect: DB_DIALECT,
 })
 
 export default db;
