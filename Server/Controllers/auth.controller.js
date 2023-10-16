afbdab98ad7541391021bc08304745bf929eeaf4
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-10-16 13:53:47 +0200

    feat(routes) add-auth.routes-functionalites

--- Server/Controllers/auth.controller.js
+++ Server/Controllers/auth.controller.js
@@ -61,3 +61,5 @@ const  authController = {
 
 }
 };
+
+export default authController;
--- Server/app.js
+++ Server/app.js
@@ -3,7 +3,7 @@ import cors from "cors";
 import userRoutes from "./Routes/users.routes.js";
 import ageRoutes from "./Routes/age.routes.js";
 import productsRoutes from "./Routes/products.routes.js"
-
+import authRoutes from "./Routes/auth.routes.js"
 export const app = express()
 app.get('/', (req , res) =>{
     res.send('<h1>Hola Backend</h1>')
@@ -15,3 +15,4 @@ app.use(express.json())
 app.use("/playmontessori/users", userRoutes);
 app.use("/playmontessori/ages", ageRoutes);
 app.use("/playmontessori/products", productsRoutes);
+app.use("/playmontessori", authRoutes);
