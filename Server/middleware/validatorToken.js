53ea16b35a60f514b5167ef3d682952639b66207
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-10-18 10:54:29 +0200

    fix(validatorToken) isAdmin-middleware-error-fixed

--- Server/middleware/validatorToken.js
+++ Server/middleware/validatorToken.js
@@ -11,8 +11,8 @@ export const verifyToken = async (req,res,next) => {
         }
         const decodedToken = jwt.verify (token, tokenSecret);
         console.log(decodedToken)
-        req.userId = decodedToken.id;
-        const user = await User.findOne (req.userId);
+        req.userId = decodedToken.userId;
+        const user = await User.findByPk (req.userId);
         if (!user){
             return res.status(404).json ({message:"user not found"})
         }
@@ -25,18 +25,24 @@ export const verifyToken = async (req,res,next) => {
 export const isAdmin = async (req, res, next) => {
     try {
         const token = req.headers["x-access-token"];
+
         const decodedToken = jwt.verify(token, tokenSecret);
-        console.log(decodedToken);
-        req.userId = decodedToken.id;
-        const user = await User.findOne(req.userId);
-        console.log(user)
-        const roles = await Roles.findOne({ where: { id: user.rolesId} });
-        console.log(roles)
+
+        console.log(decodedToken.userId);
+
+        req.userId = decodedToken.userId;
+
+        const user = await User.findByPk(req.userId);
+
+        const roles = await Roles.findByPk(user.rolesId);
+        
+
         if ( user.id === req.userId && roles.user_role === "admin") {
             next();
         } else {
             res.status(403).json({ error: 'Acceso denegado' });
         }
+        
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
