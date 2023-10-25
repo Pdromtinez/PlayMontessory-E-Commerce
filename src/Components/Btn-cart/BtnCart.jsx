5b7f98911bf882fc0c80058414a0685a28d056d7
Jassed Martinez <jassedgmartinez@gmail.com>
2023-10-25 16:30:40 +0200

    feat(cart)added alert

--- src/Components/Btn-cart/BtnCart.jsx
+++ src/Components/Btn-cart/BtnCart.jsx
@@ -1,19 +1,47 @@
-import '../Counter/counter.css'
-import React, { useState } from 'react';
-function BtnCart({initialCount, onUpdate}) {
+import './Cart.css'
+import React, { useState, useEffect } from 'react';
+function BtnCart({ initialCount, onUpdate }) {
   const [count, setCount] = useState(initialCount);
+  const [animation, setAnimation] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+
   const handleButtonClick = (operation) => {
- if (operation === 'decrement' && count>0) {
-          const newCount = count - 1;
-            setCount(newCount);
-            onUpdate (newCount);
+    if (operation === 'decrement' && count > 0) {
+      const newCount = count - 1;
+      setCount(newCount);
+      onUpdate(newCount);
+      setAnimation(true); // Activa la animación
+      setShowAlert(true); // Muestra la alerta
     }
   };
+
+  useEffect(() => {
+    if (animation) {
+      const timeout = setTimeout(() => {
+        setAnimation(false);
+      }, 1000); // Desactiva la animación después de 1 segundo
+      return () => clearTimeout(timeout);
+    }
+  }, [animation]);
+
+  useEffect(() => {
+    if (showAlert) {
+      const timeout = setTimeout(() => {
+        setShowAlert(false);
+      }, 2000); // Oculta la alerta después de 2 segundos
+      return () => clearTimeout(timeout);
+    }
+  }, [showAlert]);
+
   return (
-    <div className='Counter'>
-      <button  className="btn-counter" onClick={() => handleButtonClick('decrement')} > Buy Now </button>
+    <div className={`Counter ${animation ? 'animate' : ''}`}>
+      <button className="buttonLogin" onClick={() => handleButtonClick('decrement')}>
+        Add to cart
+      </button>
       <span>{count}</span>
+      {showAlert && <div className="alert">Product added to cart</div>}
     </div>
   );
 }
+
 export default BtnCart;
