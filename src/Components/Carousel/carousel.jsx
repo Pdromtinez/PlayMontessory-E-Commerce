a7660cceef2153da27b94c1eaa6190c30b3c5ba3
Pdromtinez <137045122+Pdromtinez@users.noreply.github.com>
2023-10-26 23:52:59 +0200

    fix(carousel) add-config-vercel

--- src/Components/Carousel/carousel.jsx
+++ src/Components/Carousel/carousel.jsx
@@ -1,33 +1,33 @@
 import './carousel.css';
 import Carousel from 'react-bootstrap/Carousel';
-
+import img1 from '../../assets/imgCarousel/1carousel.jpg';
+import img2 from '../../assets/imgCarousel/2carousel.jpg';
+import img3 from '../../assets/imgCarousel/3carousel.jpg';
 function ProductsCarousel() {
   return (
     <Carousel>
       <Carousel.Item interval={3500}>
         <img
-          src='/src/assets/imgCarousel/1carousel.jpg'
+          src={img1}
           alt='img1'
-          className='carousel-img' // Aplicar la clase de estilo personalizado
+          className='carousel-img' 
         />
       </Carousel.Item>
       <Carousel.Item interval={3500}>
         <img
-          src='/src/assets/imgCarousel/2carousel.jpg'
+          src={img2}
           alt='img2'
-          className='carousel-img' // Aplicar la clase de estilo personalizado
+          className='carousel-img' 
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src='../src/assets/imgCarousel/3carousel.jpg'
+          src={img3}
           alt='img3'
-          className='carousel-img' // Aplicar la clase de estilo personalizado
+          className='carousel-img' 
         />
-   
       </Carousel.Item>
     </Carousel>
   );
 }
-
 export default ProductsCarousel;
